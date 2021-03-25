dfdf3a3b9b15df2235c5e5803c934f4c019e0ea8
delirycki <81227443+delirycki@users.noreply.github.com>
2021-03-25 11:19:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,80 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#zad1
+#a = "a"
+#b = "b"
+#c = 1
+#d = 2
+#e =.1
+#f =.2
+#g = 2+3j
+#h = -2j
+#print(a,b,c,d,e,f,g,h)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Zadanie 2
+# a = 5
+# b = 10
+# print(type(a))
+# dodawanie =a+b
+# print(dodawanie)
+# odejmowanie = b-a
+# print(odejmowanie)
+# mnozenie = a * b
+# print(mnozenie)
+# dzielenie =b/a
+# print(dzielenie)
+# dzielenie_calkowite = a // b
+# print(dzielenie_calkowite)
+# dzielenie_reszta =a % b
+# potega = a**b
+# print(potega)
+# potega_pow=pow(a,b)
+# print(potega_pow)
 
+#zadanie 3
+# a = 5
+# b = 10
+# a+=b
+# print(a)
+# b -=a
+# print(b)
+# a *= b
+# print(a)
+# b /= a
+# print(b)
+# a//= b
+# print(a)
+# a %= b
+# print(a)
+# a **=b
+# print(a)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Paweł')
+#zadzanie 5
+# imie = "PAWEŁ"
+# nazwisko = "KOMARNICKI"
+# print(imie.capitalize()+" "+nazwisko.capitalize())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#Zadanie 6
+# tekst_piosenki="la la la la"
+# print(tekst_piosenki.count("la"))
+
+#Zadanie 7
+# lancuch_teksu="przykładowy łańcuch testu"
+# print(lancuch_teksu[2],lancuch_teksu[-1])
+
+#Zadanie 8
+# tekst_piosenki="la la la la"
+# la = tekst_piosenki.split(" ")
+# print(la)
+
+# #Zadanie 9
+# string = "Paweł"
+# zmienoprzecinkowa= 0.44
+# szesnastkowa = hex(16)
+#
+#
+# print("zmiena string to {}\nDruga float {}\nTrzecia szesnastkowa {}".format(string,zmienoprzecinkowa,szesnastkowa))
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
